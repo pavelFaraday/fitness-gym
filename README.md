31de07f7cb5197cf20786a337e0f2e3a9586254c
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-05-26 12:18:35 +0400

    Typescript Enums

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fitness-gym
-Fitness Application - React, Typescript, Vite, Tailwind, Hero Icons, Framer Motion, Form Submit, React Anchor Link Smooth Scroll. 
+
+Fitness Application - React, Typescript, Vite, Tailwind, Hero Icons, Framer Motion, Form Submit, React Anchor Link Smooth Scroll.
+
+---
+
+![Typescript Enum](./slides/ts-enums.jpg)
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import { useState } from "react";
 import Navbar from "@/scenes/navbar";
 
+/* Typing with Enum */
+// ! enums exist after BUILD
+enum SelectefPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourClasses",
+  ContactUs = "contactus",
+}
+
+/* Typical Typing */
+/* type selectefPage2 = {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourClasses",
+  ContactUs = "contactus",
+} */
+
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectefPage>(
+    SelectefPage.Home
+  );
 
   return (
     <div className="app bg-gray-20">
